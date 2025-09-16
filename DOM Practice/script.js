5cec701309ac2308c644aa4b8334809e6aac926b
mohdanaskhan1 <anask73031@gmail.com>
2025-09-16 14:51:31 +0530

    2

--- DOM Practice/script.js
+++ DOM Practice/script.js
@@ -34,7 +34,195 @@
 // console.log(inputElement.getAttribute("type"));
 
 
-//Get Multiple items using getElements by class name
+// //Get Multiple items using getElements by class name
+// const navItems = document.getElementsByClassName("nav-item");
+// console.log(navItems); //HTMLCOllection
+// console.log(navItems[2]);
+// console.log(Array.isArray(navItems)); // False
+
+
+//Get Multiple items using getElements by QuerySelectorALl
+// const navItems = document.querySelectorAll(".nav-item");
+// console.log(navItems); //NodeList
+// console.log(navItems[2]);  //ArrayLikeObject
+// console.log(Array.isArray(navItems)); // False
+
+// //array like object indexing and length
+// let navItems = document.getElementsByTagName("a");
+// console.log(navItems); //HTMLCOllection
+
+//simple for
+// for(let i=0; i<navItems.length;i++){
+//     const navItem = navItems[i];
+//     navItem.style.backgroundColor="#fff";
+//     navItem.style.color="green";
+    
+// }
+// for of
+// for (let navItem of navItems){
+//     navItem.style.backgroundColor="#fff";
+//     navItem.style.color="green";
+//     navItem.style.fontWeight="dark";
+// }
+
+
+//forEach Method
+//cant use forEach method directly to iterate throuch HTML Collection
+// navItems = Array.from(navItems);
+// navItems.forEach(navItem => {
+//     navItem.style.backgroundColor="#fff";
+//     navItem.style.color="green";
+//     navItem.style.fontWeight="dark";
+// });
+
+
+//INNERHTML
+
+// const headline = document.querySelector(".headline");
+// console.log(headline.innerHTML);
+// headline.innerHTML="INNER HTML CHANGED";
+// console.log(headline.innerHTML);
+
+
+
+//classList, add and remove and toggle class
+// const sectionTodo = document.querySelector(".section-todo");
+// console.log(sectionTodo);
+// console.log(sectionTodo.classList);
+// sectionTodo.classList.add('bg-dark');
+// sectionTodo.classList.remove('container');
+// const ans = sectionTodo.classList.contains("container");
+// console.log(ans);
+
+// sectionTodo.classList.toggle("bg-dark");
+// sectionTodo.classList.toggle("bg-dark");
+
+
+//ADD HTML ELEMENT to page
+
+// //using innerHTML
+// const todoList = document.querySelector(".todo-list");
+// todoList.innerHTML+="<li>New Todo</li>";
+// todoList.innerHTML+="<li>Teach Students</li>";
+
+//when to use and when to not
+//use to change not to add
+
+
+
+//CREATE ELEMENT AND APPEND, PREPEND, BEFORE, AFTER, REPLACEWITH
+// const newTodoItem = document.createElement("li");
+// // // const newTodoItemText = document.createTextNode("Teach Students");
+// newTodoItem.textContent="TEACH STUDENTS";
+// // // newTodoItem.append(newTodoItemText);
+// const todoList = document.querySelector(".todo-list");
+// // // todoList.append(newTodoItem);
+// todoList.prepend(newTodoItem);
+// // console.log(newTodoItem);
+
+
+// const todo1 = document.querySelector(".todo-list li");
+// todo1.remove();
+
+
+// const newTodoItem = document.createElement("li");
+// newTodoItem.textContent="TEACH STUDENTS";
+// const todoList = document.querySelector(".todo-list");
+// todoList.before(newTodoItem);
+// todoList.after(newTodoItem);
+
+
+
+//INSERT ADJACENT ELEMENTS
+//beforebegin
+//afterbegin
+//beforeend
+//afterend
+// const todoList = document.querySelector(".todo-list ");
+// todoList.insertAdjacentHTML("beforeend","<li>Teach Students</li>");
+// todoList.insertAdjacentHTML("afterend","<li>Teach Students</li>");
+// todoList.insertAdjacentHTML("beforebegin","<li>Teach Students</li>");
+// todoList.insertAdjacentHTML("afterbegin","<li>Teach Students</li>");
+
+
+
+//CLONE NODE
+// const ul = document.querySelector(".todo-list");
+// const li = document.createElement("li");
+// li.textContent="NEW NODE";
+// const li2 = li.cloneNode(true);
+// ul.append(li);
+// ul.prepend(li2);
+
+//Some old methods to support poor IE
+//appendChild;
+//insertBefore;
+//replaceChild;
+//removeChild;
+// const ul = document.querySelector(".todo-list");
+// const li = document.createElement("li");
+// const referenceNode = document.querySelector(".first-todo");
+// li.textContent="NEW NODE";
+// ul.appendChild(li);
+// ul.insertBefore(li,referenceNode);
+// ul.replaceChild(li,referenceNode);
+// ul.removeChild(li);
+
+
+
+//STATIC LIST AND LIVE LIST
+
+//query selector static list
+//getElementsBySomething live list
+
+// const listItems = document.querySelectorAll(".todo-list li");
+// const sixthLi = document.createElement("li");
+// sixthLi.textContent="todo  6";
+// const ul = document.querySelector(".todo-list");
+// ul.append(sixthLi);
+// console.log(listItems); //static list
+
+// const ul = document.querySelector(".todo-list");
+// const listItems = ul.getElementsByTagName("li");
+// const sixthLi = document.createElement("li");
+// sixthLi.textContent="todo  6";
+// ul.append(sixthLi);
+// console.log(listItems); //live list
+// console.log(listItems.length);
+// console.log(listItems[5]);
+// console.log(listItems[0]);
+// console.log(listItems[1]);
+//listItems is updated automatically when we add or remove an element from the DOM  
+
+
+//how to get dimensions of an element
+// const sectionTodo = document.querySelector(".section-todo");
+// const info = sectionTodo.getBoundingClientRect();
+// console.log(info);
+// console.log(info.height);
+// console.log(info.width);
+// console.log(info.top);
+// console.log(info.left);
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+
+
+
+//EVENTS
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ DOM Practice/script1.js
@@ -0,0 +1,36 @@
+// const rootNode = document.getRootNode();
+// const htmlElementNode = rootNode.childNodes[0];
+// console.log(htmlElementNode);
+// console.log(htmlElementNode.childNodes); // NodeList(3) [head, text, body]
+// const headElementNode = htmlElementNode.childNodes[0];
+// const textElementNode = htmlElementNode.childNodes[1];
+// const bodyElementNode = htmlElementNode.childNodes[2];
+// console.log(headElementNode);
+// console.log(textElementNode);
+// console.log(bodyElementNode);
+// //CHILD PARENT RELATION
+
+// //SIBLINGS
+// console.log(headElementNode.nextSibling);
+// console.log(headElementNode.nextSibling.textContent);
+// console.log(headElementNode.nextElementSibling)
+// console.log(headElementNode.childNodes);
+
+
+
+// const h1 = document.querySelector("h1");
+// const body = h1.parentNode.parentNode;
+// body.style.color="grey";
+// body.style.backgroundColor="Black";
+
+// const body = document.body;
+// console.log(body);
+
+// const container = document.querySelector(".container");
+// console.log(container);
+// console.log(container.childNodes);
+// console.log(container.children);
+
+
+
+
