c02b08fe2d4d68e917b51c07b47ebd465aac28c4
mohdanaskhan1 <anask73031@gmail.com>
2025-09-16 10:21:25 +0530

    1

--- DOM Practice/script.js
+++ DOM Practice/script.js
@@ -1,1 +1,49 @@
-//  
+// //getElementById    
+// let mainHeading = document.getElementById("main-heading");
+// console.log(mainHeading);
+// console.log(mainHeading.textContent);
+// console.log(mainHeading.innerText);
+
+// const mainH = document.querySelector("#main-heading");
+// console.log(mainH);
+// const header = document.querySelector(".header");
+// console.log(header);
+// const navItem = document.querySelector(".nav-item");
+// console.log(navItem);
+// const navItem1 = document.querySelectorAll(".nav-item");
+// console.log(navItem1);
+// const div = document.querySelector("div.headline h2");
+// console.log(div);
+
+
+// mainHeading.textContent="Changed Content";
+
+// const mainHeading = document.querySelector("div.headline h2");
+// mainHeading.style.color = "grey";
+// mainHeading.style.backgroundColor="blue";
+// mainHeading.style.padding="10px";
+// mainHeading.style.borderRadius="10px";
+
+// const link = document.querySelector("a");
+// console.log(link.getAttribute("href").slice(1));
+// link.setAttribute("href","https://google.com/"); 
+// console.log(link.getAttribute("href"));
+
+
+// const inputElement = document.querySelector(".form-todo input");
+// console.log(inputElement.getAttribute("type"));
+
+
+//Get Multiple items using getElements by class name
+
+
+
+
+
+
+
+
+
+
+
+
